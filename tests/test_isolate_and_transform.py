a838d553616496b4e1f8398475fce86702dc0515
psferguson <petersferguson@gmail.com>
2023-09-05 11:19:07 -0700

    unit test added

--- tests/test_isolate_and_transform.py
+++ tests/test_isolate_and_transform.py
@@ -7,33 +7,52 @@ import tempfile
 
 import lsst.utils
 
-from lsst.the.monster import MatchAndTransform, 
+from lsst.the.monster import MatchAndTransform 
 from test_catalog_measurement import TestGaiaDR3Info, TestGaiaXPInfo
+import fitsio
 
 
 class MonsterMatchAndTransformTest(lsst.utils.tests.TestCase):
-    def check_splinefitter(self, mag_offset=0.0, check_pars=True):
-        """Check the spline fitter, with optional magnitude offset.
+    def setUp(self):
+            self.GaiaDR3CatInfoClass = TestGaiaDR3Info
+            self.TargetCatInfoClass = TestGaiaXPInfo
+            self.TargetCatInfoClass.name = "GaiaXP"
+            self.outputColumns= ('id',
+                                'coord_ra',
+                                'coord_dec',
+                                'decam_g_flux_from_GaiaXP',
+                                'decam_r_flux_from_GaiaXP',
+                                'decam_i_flux_from_GaiaXP',
+                                'decam_z_flux_from_GaiaXP',
+                                'decam_y_flux_from_GaiaXP')
 
-        Parameters
-        ----------
-        mag_offset : `float`, optional
-            Overall magnitude offset between ref and meas catalogs.
-        check_pars : `bool`, optional
-            Check the parameter values? (Only works well if mag_offset=0.0).
+    def test_MatchAndTransform(self):
         """
-        def setUp(self):
-        use test gaia_Dr3 data
-        TestGaiaDR3Info, TestGaiaXPInfo
-        
-        
-        check shape and columns in output
+        Test the MatchAndTransform function, by checking the 
+        length of the output and the output columns. 
+
+        Args:
+            htmid (int): The HTM ID.
+            catalog_list (list of str): The list of catalog names.
+            write_path (str): The path to write the output file.
+        """
+
+        # Set up the test.
+        htmid = 147267
+        MAT = MatchAndTransform()
         with tempfile.TemporaryDirectory() as temp_dir:
             os.chdir(temp_dir)
-            
-            
-            run(self, 
-            htmid=None, 
-            catalog_list=[GaiaXPInfo, SkyMapperInfo, PS1Info, VSTInfo],
-            write_path=None
-    ):
+
+            # Run the MatchAndTransform function.
+            MAT.run(htmid=htmid,
+                    catalog_list=[self.TargetCatInfoClass],
+                    write_path=temp_dir
+                    )
+
+            # Read the output file.
+            output = fitsio.read(os.path.join(temp_dir, str(htmid) + ".fits"))
+
+            # Check the output.
+            self.assertEqual(output.shape, (340,))
+            self.assertEqual(output.dtype.names, self.outputColumns)
+
