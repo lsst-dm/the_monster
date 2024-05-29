34cf1475af6dc139ac1ca1f0f69d9d94fc1e44ff
Eli Rykoff <erykoff@stanford.edu>
2024-05-29 10:55:33 -0700

    Add SLR vs SDSS offset map to executable.

--- bin.src/measure_uband_offsets.py
+++ bin.src/measure_uband_offsets.py
@@ -3,10 +3,14 @@
 from lsst.the.monster import UBandOffsetMapMaker, SDSSInfo, GaiaXPuInfo
 
 
-measurer_sdss = UBandOffsetMapMaker(uband_ref_class=SDSSInfo, catalog_info_class_list=[GaiaXPuInfo])
-fname = measurer_sdss.measure_uband_offset_map_direct()
-measurer_sdss.plot_uband_offset_maps(fname, mode="direct")
+measurer_sdss_direct = UBandOffsetMapMaker(uband_ref_class=SDSSInfo, catalog_info_class_list=[GaiaXPuInfo])
+fname = measurer_sdss_direct.measure_uband_offset_map_direct()
+measurer_sdss_direct.plot_uband_offset_maps(fname, "uslr-usdss", mode="direct")
 
-measurer = UBandOffsetMapMaker()
-fname = measurer.measure_uband_offset_map()
-measurer.plot_uband_offset_maps(fname)
+measurer_xp = UBandOffsetMapMaker()
+fname = measurer_xp.measure_uband_offset_map()
+measurer_xp.plot_uband_offset_maps(fname, "uslr-uxp")
+
+measurer_sdss = UBandOffsetMapMaker(uband_ref_class=SDSSInfo)
+fname = measurer_sdss.measure_uband_offset_map()
+measurer_sdss.plot_uband_offset_maps(fname, "uslr-usdss")
