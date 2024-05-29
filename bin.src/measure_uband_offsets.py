fe50917253c8eccda723454a210a470bbe1b3c43
Eli Rykoff <erykoff@stanford.edu>
2024-05-29 10:02:53 -0700

    Fix call yet again.

--- bin.src/measure_uband_offsets.py
+++ bin.src/measure_uband_offsets.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from lsst.the.monster import UBandOffsetMapMaker, SDSSInfo
+from lsst.the.monster import UBandOffsetMapMaker, SDSSInfo, GaiaXPuInfo
 
 
-measurer_sdss = UBandOffsetMapMaker(uband_ref_class=SDSSInfo)
+measurer_sdss = UBandOffsetMapMaker(uband_ref_class=SDSSInfo, catalog_info_class_list=[GaiaXPuInfo])
 fname = measurer_sdss.measure_uband_offset_map_direct()
 measurer_sdss.plot_uband_offset_maps(fname, mode="direct")
 
