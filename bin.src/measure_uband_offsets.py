9f4b1e462ed80e5a44a4e8b22cfb09a5954f3d6b
Eli Rykoff <erykoff@stanford.edu>
2024-05-29 09:22:35 -0700

    Update uband offset map executable for direct comparison.

--- bin.src/measure_uband_offsets.py
+++ bin.src/measure_uband_offsets.py
@@ -5,7 +5,7 @@ from lsst.the.monster import UBandOffsetMapMaker, SDSSInfo
 
 measurer_sdss = UBandOffsetMapMaker(uband_ref_class=SDSSInfo)
 fname = measurer_sdss.measure_uband_offset_map()
-measurer_sdss.plot_uband_offset_maps(fname)
+measurer_sdss.plot_uband_offset_maps(fname, mode="direct")
 
 measurer = UBandOffsetMapMaker()
 fname = measurer.measure_uband_offset_map()
