96bd02065cffe2bfb2d50f23a0ed6a1b8c40cd84
Eli Rykoff <erykoff@stanford.edu>
2024-05-29 09:57:14 -0700

    Fix call again.

--- bin.src/measure_uband_offsets.py
+++ bin.src/measure_uband_offsets.py
@@ -4,7 +4,7 @@ from lsst.the.monster import UBandOffsetMapMaker, SDSSInfo
 
 
 measurer_sdss = UBandOffsetMapMaker(uband_ref_class=SDSSInfo)
-fname = measurer_sdss.measure_uband_offset_direct()
+fname = measurer_sdss.measure_uband_offset_map_direct()
 measurer_sdss.plot_uband_offset_maps(fname, mode="direct")
 
 measurer = UBandOffsetMapMaker()
