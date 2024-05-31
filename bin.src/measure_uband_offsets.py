41c7896afa6343a8f4c0b1c5f0a3d66a916f6483
Eli Rykoff <erykoff@stanford.edu>
2024-05-31 16:30:47 -0700

    Change u band offset map settings.

--- bin.src/measure_uband_offsets.py
+++ bin.src/measure_uband_offsets.py
@@ -3,14 +3,17 @@
 from lsst.the.monster import UBandOffsetMapMaker, SDSSInfo, GaiaXPuInfo
 
 
+# This is a direct map, XPu (color corrected) vs SDSS u.
 measurer_sdss_direct = UBandOffsetMapMaker(uband_ref_class=SDSSInfo, catalog_info_class_list=[GaiaXPuInfo])
 fname = measurer_sdss_direct.measure_uband_offset_map_direct()
-measurer_sdss_direct.plot_uband_offset_maps(fname, "uslr-usdss", mode="direct")
+measurer_sdss_direct.plot_uband_offset_maps(fname, "uxp-usdss", mode="direct")
 
-measurer_xp = UBandOffsetMapMaker()
-fname = measurer_xp.measure_uband_offset_map()
-measurer_xp.plot_uband_offset_maps(fname, "uslr-uxp")
-
+# This is a combined map, DES++ SLR vs SDSS u.  (SDSS footprint)
 measurer_sdss = UBandOffsetMapMaker(uband_ref_class=SDSSInfo)
 fname = measurer_sdss.measure_uband_offset_map()
 measurer_sdss.plot_uband_offset_maps(fname, "uslr-usdss")
+
+# This is a combined map, DES++ SLR vs XP u.  (Full sky)
+measurer_xp = UBandOffsetMapMaker(uband_ref_class=GaiaXPuInfo)
+fname = measurer_xp.measure_uband_offset_map()
+measurer_xp.plot_uband_offset_maps(fname, "uslr-uxp")
